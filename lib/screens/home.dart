18d20ebffc20fbd36ba75485473c93deb0f1f954
Imasha Kawmudu <133482141+Lathu-Ima@users.noreply.github.com>
2024-02-27 01:00:53 +0530

    Home Page Adding
    Finalizing home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,25 +1,155 @@
 import 'package:flutter/material.dart';
+import 'activity_log.dart';
+import 'help_and_support.dart';
+import 'navbar.dart';
+import 'settings.dart'; // Assuming you have a SettingsPage
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final String username;
 
   const HomePage({Key? key, required this.username}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: const Text('Home Page'),
+        backgroundColor: const Color.fromRGBO(10, 39, 66, 1),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text(
+              widget.username,
+              style: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'Port Lligat Slab',
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            IconButton(
+              icon: const Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SettingsPage()),
+                );
+              },
+            ),
+          ],
+        ),
       ),
-      body: Center(
-        child: Text(
-          'Welcome, $username!',
-          style: const TextStyle(
-            fontSize: 40,
-            fontFamily: 'Port Lligat Slab',
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.only(top: 20),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50),
+                  topRight: Radius.circular(50),
+                ),
+              ),
+              child: GridView.count(
+                crossAxisCount: 2,
+                children: [
+                  ItemHome(
+                    title: 'Activity Log',
+                    icon: Icons.assignment,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ActivityLogPage()),
+                      );
+                    },
+                  ),
+                  ItemHome(
+                    title: 'Favorites',
+                    icon: Icons.favorite,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ActivityLogPage()),
+                      );
+                    },
+                  ),
+                  ItemHome(
+                    title: 'Settings',
+                    icon: Icons.settings,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SettingsPage()),
+                      );
+                    },
+                  ),
+                  ItemHome(
+                    title: 'Help',
+                    icon: Icons.help,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const HelpAndSupportPage()),
+                      );
+                    },
+                  ),
+                  ItemHome(
+                    title: 'About Us',
+                    icon: Icons.info,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const HelpAndSupportPage()),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
           ),
+        ],
+      ),
+      bottomNavigationBar: const BottomNavBar(),
+    );
+  }
+}
+
+class ItemHome extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final VoidCallback onTap;
+
+  const ItemHome(
+      {Key? key, required this.title, required this.icon, required this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, size: 50, color: Colors.blue),
+            const SizedBox(height: 10),
+            Text(title,
+                style:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+          ],
         ),
       ),
     );
